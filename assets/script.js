c5a0480756ca29e7f8b60316f9c4a0cd793b1faa
Nicole-Hong <n.hong@queensu.ca>
2023-07-03 11:41:29 -0700

    add Javascript code - if stmt & for loop - to scripts.js

---
+++ assets/script.js
@@ -0,0 +1,70 @@
+// JS Hands-on from Prework Mode3-3
+
+
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+//console.log(topics);
+
+/*
+var topic = "HTML";
+
+if (topic === "HTML") {
+    console.log("Let's study HTML!");
+} else if (topic === "CSS") {
+    console.log("Let's study CSS!");
+} else if (topic === "Git") {
+    console.log("Let's study Git!");
+} else if (topic === "JavaScript") {
+    console.log("Let's atusy JavaScript!");
+} else {
+    console.log("Please try again!");
+}
+*/
+
+for(var n = 0; n < topics.length; n++) {
+    console.log(topics[n]);
+}
+
+
+//var shape = "circle";
+//console.log(shape);
+//var shapes = ["triangle", "square", "pentagon", "circle"];
+
+/*
+console.log(shapes[0]);
+console.log(shapes[1]);
+console.log(shapes[2]);
+console.log(shapes[3]);
+*/
+
+/*
+for(var x=0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
+*/
+
+
+
+/*
+console.log(topics);
+var topics = "HTML, CSS, Git, JavaScript";
+*/
+
+/*
+if (0) {
+    console.log("I'm 0!")
+ } else if (null) {
+    console.log("I'm null!")
+ } else if (true) {
+    console.log("I'm true!")
+ }
+*/
+
+/*
+function multiply(num1, num2){
+    let result = num1 * num2;
+    return result;
+}
+
+console.log(multiply(4,7))
+*/
+
