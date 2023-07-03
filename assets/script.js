b2c81953b8f30f33083216b6cc502aec08d2ca31
Nicole-Hong <n.hong@queensu.ca>
2023-07-03 12:10:12 -0700

    Update JavaScript code in scripts.js - control flow, functions and observing the result through Chrome DevTools

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,16 @@
 // JS Hands-on from Prework Mode3-3
 
+function openRemark() {
+    console.log("Here are the topics we learned through Prework:");
+}
+
+openRemark();
+
 
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+
 //console.log(topics);
 
 /*
@@ -20,10 +29,11 @@ if (topic === "HTML") {
 }
 */
 
+/*
 for(var n = 0; n < topics.length; n++) {
     console.log(topics[n]);
 }
-
+*/
 
 //var shape = "circle";
 //console.log(shape);
@@ -67,4 +77,42 @@ function multiply(num1, num2){
 
 console.log(multiply(4,7))
 */
+
+/*
+function helloWorld() {
+    // code block
+    console.log("Hello World! I am a function!")
+}
+
+helloWorld();
+*/
+
+function listTopics() {
+    for (var x = 0; x< topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
+
+
+function selectTopic() {
+    if (randomTopic === "HTML") {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === "CSS") {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === "Git") {
+        console.log("Let's study Git!");
+    } else if (randomTopic === "JavaScript") {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log("Please try again!");
+    }
+}
+
+
+listTopics();
+
+console.log("Which topic should we study first?");
+
+selectTopic();
+
 
